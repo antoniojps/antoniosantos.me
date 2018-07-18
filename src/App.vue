1ede572339c249d1e103f74ff3b02786ecdac0c4
António Santos <antoniojps@ua.pt>
2018-07-18 18:14:34 +0100

    add comment explanation for key in router-view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <!-- $route.fullPath tells Vue that if the
+    fullPath changes even if the component
+    is the same you should render it from scratch -->
     <router-view :key="$route.fullPath" />
   </div>
 </template>

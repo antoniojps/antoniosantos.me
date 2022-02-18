468acdabc7105da8b1a7934f1249ea8ccdf9b75b
antoniojps <antonio.santos@mindera.com>
2022-02-18 17:22:22 +0000

    add content links

--- src/components/ContentGrid/index.tsx
+++ src/components/ContentGrid/index.tsx
@@ -4,37 +4,50 @@ import ImgCardUa from "../../../public/card-ua.png";
 import ImgCardTvg from "../../../public/card-tvg.png";
 import ImgCardYtrank from "../../../public/card-ytrank.png";
 import { Grid, A, B, C } from "./styles";
+import Link from "next/link";
 
 export const ContentGrid = () => {
   return (
     <Grid>
       <A>
-        <Image
-          src={ImgCardYtrank}
-          alt="Youtube Rank Checker Website Mockup"
-          layout="responsive"
-          quality={100}
-          placeholder="blur"
-        />
+        <Link href="/portfolio#ytrank" passHref>
+          <a>
+            <Image
+              src={ImgCardYtrank}
+              alt="Youtube Rank Checker Website Mockup"
+              layout="responsive"
+              quality={100}
+              placeholder="blur"
+            />
+          </a>
+        </Link>
       </A>
       <B>
-        <Image
-          src={ImgCardTvg}
-          alt="Mobile phone with horse racing app"
-          layout="responsive"
-          quality={100}
-          placeholder="blur"
-        />
+        <Link href="/portfolio#tvg" passHref>
+          <a>
+            <Image
+              src={ImgCardTvg}
+              alt="Mobile phone with horse racing app"
+              layout="responsive"
+              quality={100}
+              placeholder="blur"
+            />
+          </a>
+        </Link>
       </B>
       <C>
-        <Image
-          src={ImgCardUa}
-          alt="Two pages of the UA CMS"
-          layout="responsive"
-          priority
-          quality={100}
-          placeholder="blur"
-        />
+        <Link href="/portfolio#editor-custom-cms-for-the-university-of-aveiro" passHref>
+          <a>
+            <Image
+              src={ImgCardUa}
+              alt="Two pages of the UA CMS"
+              layout="responsive"
+              priority
+              quality={100}
+              placeholder="blur"
+            />
+          </a>
+        </Link>
       </C>
     </Grid>
   );

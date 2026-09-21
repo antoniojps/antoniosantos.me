b62c32a00a7ae5ca3909fe7b09da6c3bc195bae7
Antonio Santos <antonio@tubelab.net>
2026-09-21 01:38:49 +0100

    feat: redirect /tdw-02 to talk document

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/tdw-02",
+        destination:
+          "https://docs.google.com/document/d/1k-E6nEgFC7x0uEEBHS5NA4o34njF1px7/edit?usp=sharing&ouid=103011433578771485622&rtpof=true&sd=true",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
